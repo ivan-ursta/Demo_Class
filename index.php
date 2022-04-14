8e9f0c8e7a29bb141804e72d2f0b4eb9e55d983b
ivan-ursta <ursta2017@gmail.com>
2022-04-14 16:26:14 +0300

    Fix Creator and Concrete Creator class

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ abstract class Animal
     {
         //Call the factory method to create a Product object...
         $animal = $this->getAnimal();
-        $animal->showAnimal();
+        $animal->createAnimal($animal);
     }
 }
 //Concrete Creator
@@ -29,7 +29,7 @@ class Terrestrial extends Animal
 }
 
 //Concrete Creator
-class Aquatic
+class Aquatic extends Animal
 {
     private $name;
 
@@ -44,7 +44,7 @@ class Aquatic
 }
 
 //Concrete Creator
-class Birds
+class Birds extends Animal
 {
     private $name;
 
@@ -57,6 +57,51 @@ class Birds
         return new BirdsConnector($this->name);
     }
 }
+//Product interface
+interface AnimalConnector
+{
+    public function createAnimal($animal): void;
+}
+//Concrete Product
+class TerrestrialConnector implements AnimalConnector
+{
+    private $name;
+    public function __construct(string $n)
+    {
+        $this->name = $n;
+    }
+    public function createAnimal($animal): void
+    {
+        echo "Send request to create Terrestrial animal.\n";
+    }
+}
+//Concrete Product
+class AquaticConnector implements AnimalConnector
+{
+    private $name;
+    public function __construct(string $n)
+    {
+        $this->name = $n;
+    }
+    public function createAnimal($animal): void
+    {
+        echo "Send request to create Aquatic animal.\n";
+    }
+}
+
+class BirdsConnector implements AnimalConnector
+{
+    private $name;
+    public function __construct(string $n)
+    {
+        $this->name = $n;
+    }
+    public function createAnimal($animal): void
+    {
+        echo "Send request to create Birds animal.\n";
+    }
+}
+
 
 $r=new Rabbit('rabbit','jump','Robby');
 $r->Show();
